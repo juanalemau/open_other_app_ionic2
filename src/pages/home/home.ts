fb7f1b84864c37bd96d12a35b8cf136132de1bb4
BCP COELAB <bcpcoelab@MacBook-Pro-de-BCP.local>
2018-02-15 15:27:01 -0500

    [update] actualizacion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Device } from '@ionic-native/device';
-import { AppAvailability } from '@ionic-native/app-availability';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { NavController } from 'ionic-angular';
 
@@ -12,35 +10,12 @@ export class HomePage {
 
   constructor(
   	public navCtrl: NavController,
-  	public Device: Device,
-  	public InAppBrowser: InAppBrowser,
-  	public AppAvailability: AppAvailability) {
+  	public appBrowser: InAppBrowser) {
 
   }
-
-  launchExternalApp(iosSchemaName: string, androidPackageName: string, appUrl: string, httpUrl: string, username: string) {
-	let app: string;
-	if (Device.device.platform === 'iOS') {
-		app = iosSchemaName;
-	} else if (Device.device.platform === 'Android') {
-		app = androidPackageName;
-	} else {
-		let browser = new InAppBrowser(httpUrl + username, '_system');
-		return;
-	}
-
-	AppAvailability.check(app).then(
-		() => { // success callback
-			let browser = new InAppBrowser(appUrl + username, '_system');
-		},
-		() => { // error callback
-			let browser = new InAppBrowser(httpUrl + username, '_system');
-		}
-	);
-}
-
 
   openNutricionista(){
-  	this.launchExternalApp('fb://', 'com.facebook.katana', 'fb://profile/', 'https://www.facebook.com/', '');
+  	  this.appBrowser.create('vnd.youtube://', '_system'); // Youtube
   }
+
 }
