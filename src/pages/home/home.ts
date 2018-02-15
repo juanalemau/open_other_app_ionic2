2ef3f8b89aaaeea0a93e19d9c383181c7eb9e9bf
BCP COELAB <bcpcoelab@MacBook-Pro-de-BCP.local>
2018-02-15 14:50:02 -0500

    [update] actualizacion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InAppBrowser, AppAvailability, Device } from 'ionic-native';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -12,6 +13,29 @@ export class HomePage {
 
   }
 
+  launchExternalApp(iosSchemaName: string, androidPackageName: string, appUrl: string, httpUrl: string, username: string) {
+	let app: string;
+	if (Device.device.platform === 'iOS') {
+		app = iosSchemaName;
+	} else if (Device.device.platform === 'Android') {
+		app = androidPackageName;
+	} else {
+		let browser = new InAppBrowser(httpUrl + username, '_system');
+		return;
+	}
+
+	AppAvailability.check(app).then(
+		() => { // success callback
+			let browser = new InAppBrowser(appUrl + username, '_system');
+		},
+		() => { // error callback
+			let browser = new InAppBrowser(httpUrl + username, '_system');
+		}
+	);
+}
+
+
   openNutricionista(){
+  	this.launchExternalApp('fb://', 'com.facebook.katana', 'fb://profile/', 'https://www.facebook.com/', username);
   }
 }
