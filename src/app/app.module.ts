1edf6cbb8faaa94e404d0eae3f6ea46ac17461e7
BCP COELAB <bcpcoelab@MacBook-Pro-de-BCP.local>
2018-02-15 12:03:57 -0500

    [update] actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +25,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    LaunchNavigator,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-navigator';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+  	public navCtrl: NavController,
+  	public launchNavigator: LaunchNavigator) {
 
   }
 
+  openNutricionista(){
+    // Default handlers
+	let successCallback = function(data) {
+		alert("Success!");
+		// if calling canLaunch() with getAppList:true, data will contain an array named "appList" with the package names of applications that can handle the uri specified.
+	};
+	let errorCallback = function(errMsg) {
+		alert("Error! " + errMsg);
+	}
+
+	window.plugins.launcher.canLaunch({packageName:'com.coe.bcp.nutricionista'}, successCallback, errorCallback);
+  }
 }
